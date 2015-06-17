0589e0407043e20f883ab4222f600db9995be901
Connor Leech <connorleech@gmail.com>
2015-06-17 10:28:55 -0700

    fix toggle following, add all artists

--- ShowJunkie/www/js/services/follows.js
+++ ShowJunkie/www/js/services/follows.js
@@ -1,50 +1,27 @@
 
-app.factory('Follows', function(FIREBASE_URL, $firebaseArray, Auth){
-	var artistRef = new Firebase(FIREBASE_URL + 'artists');
-	var usersRef = new Firebase(FIREBASE_URL + 'users');
-
-	// make firebase arrays
-	var artists = $firebaseArray(artistRef);
-	
-	var users = $firebaseArray(usersRef);
+app.factory('Follows', function(FIREBASE_URL, $firebaseArray){
 
 	return {
-		addArtist: function(artistId){
-			var followsRef = new Firebase(FIREBASE_URL + 'follows/' + artistId);
-			var artistFollows = $firebaseArray(followsRef);
+		followArtist: function(artistName, curr_email){
+			var artistFollowRef = new Firebase(FIREBASE_URL + 'following/' + artistName);
+			var artistFollowing = $firebaseArray(artistFollowRef);
 
-			follows.$add({
-				artistId: artistId,
-				followers: []
-			});
+        	artistFollowing.$add(curr_email);
 		},
-		addFollowerToArtist: function(artistId, userId){
-			artists.$keyAt(artistId)
+		unfollowArtist: function(artistName, curr_email){
+			var artistFollowRef = new Firebase(FIREBASE_URL + 'following/' + artistName);
+			var artistFollowing = $firebaseArray(artistFollowRef);
+
+			artistFollowing
+				.$loaded()
+				.then(function(follower){
+	        		for(var key in follower){
+	            		if(follower[key].$value === curr_email){
+	              			// remove email from follow list
+	              			artistFollowing.$remove(follower[key]);
+	            		}
+	          		}
+        		});
 		}
 	};
 });
-
-
-// return User.getCurrentUserSnapshot()
-//   .on("value", function(snapshot){
-//     // gets current user
-//     var userObj = snapshot.val();
-
-//     for(var userId in userObj){
-    
-//       // refernece current user's data in firebase
-//       var thisUserRef = new Firebase(FIREBASE_URL + 'users/' + userId);
-//       var user = $firebaseObject(thisUserRef);
-
-//       // create new following list
-//       var following = angular.copy(userObj[userId].following);
-//       following.push(artistName);
-//       user.following = following;
-
-//       console.log(following);
-
-//       // send updated following list to firebase
-//       user.$save();
-
-//     }
-//   });
