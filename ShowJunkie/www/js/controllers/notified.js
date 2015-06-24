392c82d4d36172009b7112efe7957ab231f31e5f
Clay Shaw <Claydshaw@gmail.com>
2015-06-24 08:28:08 -0700

    testing accordion

--- ShowJunkie/www/js/controllers/notified.js
+++ ShowJunkie/www/js/controllers/notified.js
@@ -1,13 +1,14 @@
-app.controller('notifiedCtrl', function ($scope, Notified, $rootScope, accordian){
+app.controller('notifiedCtrl', function ($scope, Notified, $rootScope){
 
 	Notified.getFollowingArtists().then(function(followList){
-
+		// 
 		Notified.getEventsForArtists(followList).then(function(events_for_user){
 			$rootScope.myEvents = events_for_user;
 			$rootScope.count = events_for_user.length;
 			console.log(events_for_user);
 		})
 	});
+	// Show more counter
 	$scope.feedLimit = 10;
 	$scope.increaseFeed = function(){
 		$scope.feedLimit += 10;
@@ -15,6 +16,18 @@ app.controller('notifiedCtrl', function ($scope, Notified, $rootScope, accordian
 		if($scope.feedLimit >= $rootScope.myEvents.length){
 			$scope.feedLimit = $rootScope.myEvents.length;
 		}
-		
+	};
+	// Accordion for longer description reading
+
+	$scope.toggleGroup = function (group) {
+		if ($scope.isGroupShown(group)) {
+			$scope.shownGroup = null;
+		} else {
+			$scope.shownGroup = group;
+		}
+	};
+
+	$scope.isGroupShown = function (group) {
+		return $scope.shownGroup === group;
 	};
 });
